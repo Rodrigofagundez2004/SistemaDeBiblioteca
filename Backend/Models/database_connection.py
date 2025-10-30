18060ba92d7e595560e8c7bd4711fb0063676895
Rodrigofagundez2004 <yeekko19@gmail.com>
2025-10-30 16:01:01 -0300

    ModelParticipante

--- Backend/Models/database_connection.py
+++ Backend/Models/database_connection.py
@@ -1,36 +1,14 @@
 import mysql.connector
-from mysql.connector import Error
-import os
-from dotenv import load_dotenv
-
-load_dotenv() #carga las variables de entorno desde el archivo .env
-class DatabaseConnection:
-    @staticmethod
-    def get_connection():
-        try:
-            connection = mysql.connector.connect(
-                host.getenv("DB_HOST"
-                , database=os.getenv("DB_NAME")
-                , user=os.getenv("DB_USER")
-                , password=os.getenv("DB_PASSWORD")
-                , port=os.getenv("DB_PORT")
-                
-            )
-            print("Connection to database established")
-            return connection
-            except Error as e:
-            print(F"Error while connecting to database: {e}")
-            return None
-            
-    @staticmethod
-    def execute_query(query, params=None):
-        connection = None
-        cursor = None
-        try:
-            connection = DatabaseConnection.get_connection()
-            if connection is None:
-                return None
-            cursor = connection.cursor(dictionary=True)
-            cursor.execute(query, params or ())
-
-       
+def connection_db():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user= "root",
+        password="Agusrodpro1",
+        database="SistemaDeBiblioteca",
+        port=3306
+    )
+    if mydb.is_connected():
+        print("Conexion Sin errores")
+        return mydb
+    else:
+        print("Error en la conexion")   
---
+++ Backend/Models/participante_model.py
@@ -0,0 +1,79 @@
+from .database import Database
+import mysql.connector
+
+class ParticipanteModel:
+    def get_all(self):
+        db = Database()
+        conn = db.get_connection()
+        cursor = None
+        try:
+            cursor = Database()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT *FROM participante")
+            return cursor.fetchall()
+        except Exception as e:
+            print(f"Error al obtener participantes: {e}")
+            return []
+        finally:
+            if cursor: cursor.close()
+            if conn: conn.close()
+    def get_by_ci(self, ci):
+        db = Database()
+        conn = db.get_connection()
+        cursor = None
+        try:
+            cursor = conn.cursor(dictionary= True)
+            cursor.execute("SELECT * FROM participante WHERE ci = %s", (ci,))
+            return cursor.fetchone()
+        except Exception as e:
+            print(f"Error al obtener participante por CI: {e}")
+            return None
+        finally:
+            if cursor: cursor.close()
+            if conn: conn.close()
+    def create(self, ci, nombre, apellido, telefono, email):
+        db = Database()
+        conn = db.get_connection()
+        cursor = None
+        try:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO participante (ci, nombre, apellido, telefono, email) VALUES (%s, %s, %s, %s, %s)", (ci, nombre, apellido, telefono, email))
+            conn.commit()
+            return True, "Paricipante creado de forma exitosa."
+        except mysql.connector.IntegrityError as e:
+            return False,  "Error: Ya existe un participante con esa cédula o email"
+        except Exception as e:
+            return False, f"Error al crear participante: {e}"
+        finally:
+            if cursor: cursor.close()
+            if conn: conn.close()
+    def update(self, ci, nombre, apellido, email):
+        db = Database()
+        conn = db.get_connection()
+        cursor = None
+        try:
+            cursor = conn.cursor()
+            cursor.execute("UPDATE participante SET nombre = %s, apellido = %s, email = %s WHERE ci = %s", (nombre, apellido, email, ci))
+            conn.commit()
+            return cursor.rowcount > 0
+        except Exception as e:
+            print(f"Error al actualizar participante: {e}")
+            return False
+        finally:
+            if cursor: cursor.close()
+            if conn: conn.close()
+    def delete(self, ci):
+        db = Database()
+        conn = db.get_connection()
+        cursor = None
+        try:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM participante WHERE ci = %s", (ci,))
+            conn.commit()
+            return cursor.rowcount > 0
+        except Exception as e:
+            print(f"Error al eliminar participante: {e}")
+            return False
+        finally:
+            if cursor: cursor.close()
+            if conn: conn.close()
